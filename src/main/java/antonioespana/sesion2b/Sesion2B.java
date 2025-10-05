8bb07eccba8ec0a1a4142fd9f1725023a2da4a64
AntonioEspana <140651448+AntonioEspana@users.noreply.github.com>
2025-10-05 18:11:45 +0200

    Initial commit

---
+++ src/main/java/antonioespana/sesion2b/Sesion2B.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package antonioespana.sesion2b;
+
+/**
+ *
+ * @author gtaan
+ */
+public class Sesion2B {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
